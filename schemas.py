57b8f512b7057a9d8aa108cbe2fab0ddd17aaa77
Sudharsan RaviKumar <supersudh@gmail.com>
2023-12-24 16:01:11 +0530

    Tags 1. Create Tag Model 2. Create ItemTags Model 3. Ability to create, link, unlink tags 4. Associate tags with stores and items

--- schemas.py
+++ schemas.py
@@ -18,10 +18,28 @@ class ItemUpdateSchema(Schema):
     store_id = fields.Int()
 
 
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+
+
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
 
 
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
