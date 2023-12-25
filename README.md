50be29bdea67502d6f58cdaed1ffa55e2bd894de
Sudharsan RaviKumar <supersudh@gmail.com>
2023-12-25 13:57:33 +0530

    Setup psycopg2 and update README.md

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+## Venv commands
+
+```
+python3 -m venv .venv
+[Close terminal session in VSCODE and Press Cmd + J]
+pip install -r requirements.txt
+flask run
+```
+
 ## Run Dockerfile locally
 
 ### Build the image
@@ -10,4 +19,12 @@ docker build -t flask-smorest-api .
 
 ```
 docker run -dp 5005:5000 -w /app -v "$(pwd):/app" flask-smorest-api sh -c "flask run --host 0.0.0.0"
+```
+
+## Migrations
+
+```
+flask db init
+flask db migrate
+flask db upgrade
 ```
--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
-import secrets
+from dotenv import load_dotenv
 
 from db import db
 from blocklist import BLOCKLIST
@@ -16,6 +16,7 @@ from resources.user import blp as UserBluePrint
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    load_dotenv()
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
